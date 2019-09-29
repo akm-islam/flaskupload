7d4d78fc513c76e3580ba75459bf0e0ba971793f
AKM Islam <mamunjahir.com@gmail.com>
2019-09-28 20:14:03 -0400

    Multiple files upload has been added and old files delete option has activated

--- src/App.js
+++ src/App.js
@@ -1,110 +1,55 @@
+/*
+uploadtest/templates/upload.html is for testing the program with flask
+handleFile function responds to change in input field.
+handleUpload function does the upload operation
+To upload multiple files we iterate objects and add them to multi part object with the tag 'file'
+*/
 import React from 'react';
-import Lineto from "./components/lineto_test";
 import axios from 'axios';
-import {Button,Row,Col,Collapse,Navbar,NavbarToggler,NavbarBrand,Nav,NavItem,NavLink,UncontrolledDropdown,DropdownToggle,DropdownMenu,DropdownItem,Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 export default class Example extends React.Component {
   constructor(props) {
     super(props);
-    this.toggle = this.toggle.bind(this);
     this.state = {
       modal: false,
-      file:null
+      file:{}
     };
   }
   handleFile=(e)=>{
-    let file=e.target.files[0]
+    let file=e.target.files
     this.setState({file:file})
-    //console.log(e.target.files)
   }
   handleUpload=(e)=>{
     let file=this.state.file;
     let formdata=new FormData()
-    formdata.append('file',file)
+    for (var key in this.state.file) {
+    formdata.append('file',this.state.file[key])
+    }
     axios({
       url:'http://localhost:5000/uploader',
       method:"POST",
       headers:{
-        authorizaiotn:'Hello'
+      authorizition:'Hello'
       },
       data:formdata
-    }).then((res)=>{
-      console.log(res)
+    }).then((respose_from_server)=>{
+    // then is the response
+      console.log(respose_from_server.data)
     },(err)=>{
       console.log(err)
     })
   }
-  clickhandler=()=>{
-    console.log("Hello world")
-  }
-
-  toggle() {
-    this.setState(prevState => ({
-      modal: !prevState.modal
-    }));
-  }
   render() {
     return (
       <div>
-        <Navbar color="faded" light expand="sm">
-          <NavbarBrand href="/">UrbanForest</NavbarBrand>
-          <NavbarToggler/>
-          <Collapse navbar>
-            <Nav className="ml-auto" navbar>
-            <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Sort by
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Atribute
-                  </DropdownItem>
-                  <DropdownItem>
-                    Datasets
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-              <NavItem>
-                <NavLink>Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <Button color="primary" size="md" onClick={this.clickhandler}>Process</Button>
-                <Button color="primary" size="md" onClick={this.toggle}>Modal</Button>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
 {/* Row starts here */}
-        <Row>
-          <Col md="2" className="sidebar">
               <div style={{backgroundColor:"rgb(224,224,224,.3)",width:"100%",height:"400px"}}>
               <form action = "http://localhost:5000/uploader" method = "POST" >
-                <input type="file" name="fileupload" id="fileupload" onChange={(e)=>this.handleFile(e)}></input>
+                <input type="file" name="fileupload" id="fileupload" onChange={(e)=>this.handleFile(e)} multiple={true}></input>
                 <label htmlFor="fileupload"></label>
                 <button type="button" onClick={(e)=>this.handleUpload(e)}>Upload</button>
                 </form>
               </div>
-          </Col>
-          <Col md="10" style={{padding:1}}>
-              <div style={{backgroundColor:"rgb(224,224,224,.3)",width:"100%",height:"400px"}}>
-              </div>
-          </Col>
-        </Row>
 {/* Row starts here */}
-        <Modal isOpen={this.state.modal} toggle={this.toggle} backdrop={this.state.backdrop} size="lg">
-          <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
-          <ModalBody>
-          <Row>
-          <Col md="2" style={{padding:1}}>
-          <div style={{backgroundColor:"rgb(224,224,224,.3)",width:"100%",height:"400px"}}></div>
-          </Col>
-          <Col md="10" style={{padding:1}}>
-          <div style={{backgroundColor:"rgb(224,224,224,.3)",width:"100%",height:"400px"}}>
-          </div>
-          </Col>
-        </Row>
-          </ModalBody>
-        </Modal>
-
       </div>
     );
   }
--- uploadtest/01_test.py
+++ uploadtest/01_test.py
@@ -1,7 +1,7 @@
 # chmod 644 01_test.py
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
-import os
+import os,glob
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
@@ -10,14 +10,18 @@ app.config['UPLOAD_PATH']="./upload"
 @app.route('/upload')
 def upload_file1():
    return render_template('upload.html')
-	
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
+      files = glob.glob('./upload/*')
+      for f in files:
+         os.remove(f)
 # request.files conatains (werkzeug.datastructures.FileStorage)  
       for f in request.files.getlist('file'): 
 # f.save(arg1 is the path, arg2 is the filename)
+         print(f.filename)
          f.save(os.path.join(app.config['UPLOAD_PATH'],  secure_filename(f.filename)))
       return 'file uploaded successfully'
 if __name__ == '__main__':
    app.run(debug = True)
+
