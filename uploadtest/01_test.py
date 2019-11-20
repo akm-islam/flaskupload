8ad448308afac8f00ced6e5418f7af4a707c0596
AKM Islam <mamunjahir.com@gmail.com>
2019-11-19 23:36:07 -0500

    upadeted nov

--- uploadtest/01_test.py
+++ uploadtest/01_test.py
@@ -60,7 +60,7 @@ def hello_world3():
         for i in result:
             print("Result is: ",i, file=sys.stderr)
         return make_response(jsonify(result), 200)
-#------------------------------------------------- Processing first request
+#------------------------------------------------- Processing process request
 @app.route('/json',methods=['POST','GET'])
 def hello_world2():
     if(request.is_json):
@@ -70,20 +70,20 @@ def hello_world2():
         if(req.get("myrequest")=='data'):
             datasets_with_Attributes={}
             count=0;
-            max=req.get("size");
-            if(max==20):
+            type=req.get("type");
+            if(type=="uploaded"):
                 mypath='./uploaded/*.csv'
             else:
                 mypath='./upload/*.csv'
             for filename in glob.glob(mypath):
-                if(max!=20 and req.get("all")=="false" ):
+                if(type!="uploaded" and req.get("all")=="false" ):
                     if(filename[9:] in datasets):
                         if(count<150):
                             count=count+1;
                             df = pd.read_csv(filename);
                             fname=re.sub(r'.csv', '',filename[9:])
                             datasets_with_Attributes[fname]=df.columns.tolist()
-                elif(max==20):
+                elif(type=="uploaded"):
                     if(count<150):
                         count=count+1;
                         df = pd.read_csv(filename);
@@ -237,6 +237,117 @@ def stat_metric():
             numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
             for dataset in given[0]:
                 dataset2="./upload/"+dataset+".csv"
+                if(path.exists(dataset2)):
+                    data=pd.read_csv(dataset2)
+                numerical_vars = list(data.select_dtypes(include=numerics).columns)
+                data = data[numerical_vars]
+                data=data.dropna(how='all',axis='columns')
+                data=data.dropna(how='all',axis='rows')
+                data=data.fillna(value=0)
+                att1_dict={}
+                for att in given[1]:
+                    if(att in data.columns):
+                        att2_dict={}
+                        for att2 in data.columns:
+                            p=data[att]
+                            q=data[att2]
+                            kl_val=np.sum(np.where(p != 0, p * np.log(p / q), 0))
+                            if(np.isnan(kl_val)):
+                                print(kl_val)
+                            else:
+                                if((kl_val!=np.inf)):
+                                    att2_dict[att2]=kl_val
+                        att1_dict[att]=att2_dict
+                kl_dict[dataset]=att1_dict 
+            return make_response(jsonify({"kl_data":kl_dict}), 200)
+        return make_response(jsonify({"else":"something wrong"}), 200)
+    else:
+        return make_response(jsonify({"message": "Else"}), 200)
+#--------------------------------------------------------------------- Processing stat request2
+@app.route('/statmetrics2',methods=['POST','GET'])
+def stat_metric2():
+    if(request.is_json):
+        req=request.get_json();
+# ------------------------------------------------------------Prob distribution
+        if(req.get("req_for")=='prob_dist'):
+            given=req.get("given")
+            dict={}
+            for dataset in given[0]:
+                dataset2="./uploaded/"+dataset+".csv"
+                data=pd.read_csv(dataset2)
+                data=data.dropna(how='all',axis='columns')
+                data=data.dropna(how='all',axis='rows')
+                data=data.fillna(value=0)
+                total=data.shape[0]
+                dict3={}
+                for col in data.columns:
+                    if col in given[1]:
+                        dict2={} 
+                        uniq=data[col].unique()
+                        for i in uniq:
+                            dict2[str(i)]=data.loc[data[col]==i].shape[0]/total
+                            #print(i,data.loc[data[col]==i].shape[0]/total)
+                        dict3[col]=dict2
+                dict[dataset]=dict3
+            return make_response(jsonify({"prob_data":dict}), 200)
+# ------------------------------------------------------------Mutual_info
+        elif(req.get("req_for")=='mutual_info'):
+            given=req.get("given")
+            mydict1={}
+            mydict={}
+            for dataset in given[0]:
+                dataset2="./uploaded/"+dataset+".csv"
+                if(path.exists(dataset2)):
+                    data=pd.read_csv(dataset2)
+                    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+                    numerical_vars = list(data.select_dtypes(include=numerics).columns)
+                    data = data[numerical_vars]
+                    data=data.dropna(how='all',axis='columns')
+                    data=data.dropna(how='all',axis='rows')
+                    data=data.fillna(value=0)
+                for col in data.columns:
+                    if col in given[1]:
+                        numerical_vars = list(data.select_dtypes(include=numerics).columns)
+                        data = data[numerical_vars]
+                        lab=[col]
+                        X_train, X_test, y_train, y_test = train_test_split(data.drop(labels=lab, axis=1),data[col],test_size=0.1,random_state=0)
+                        mi = mutual_info_regression(X_train.fillna(0), y_train)
+                        mi = pd.Series(mi)
+                        mi.index = X_train.columns
+                        mydict[col]=mi.to_dict()
+                mydict1[dataset]=mydict            
+            return make_response(jsonify({"mutual_info_data":mydict1}), 200)
+# ---------------------------------------------------------------------------------------Correlation
+        elif(req.get("req_for")=='correlation'):
+            #print("data is ",req.get("given"), file=sys.stderr)
+            corr_dict={}
+            given=req.get("given")
+            numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+            for dataset in given[0]:
+                dataset2="./uploaded/"+dataset+".csv"
+                if(path.exists(dataset2)):
+                    data=pd.read_csv(dataset2)
+                    numerical_vars = list(data.select_dtypes(include=numerics).columns)
+                    data = data[numerical_vars]
+                    data=data.dropna(how='all',axis='columns')
+                    data=data.dropna(how='all',axis='rows')
+                    data=data.fillna(value=0)
+                    dict2={}
+                    for att in given[1]:
+                        if att in data.columns:
+                            print(att)
+                            dict2[att]=data.corr()[att].drop(att,axis=0).to_dict()
+                    corr_dict[dataset]=dict2
+                            #print("data is ",corr_dict, file=sys.stderr)
+            return make_response(jsonify({"correlation_data":corr_dict}), 200)
+# ---------------------------------------------------------------------------------------KL-Divergnce
+        elif(req.get("req_for")=='kl_div'):
+            print("Kl_div got ",req.get("given"), file=sys.stderr)
+            kl_dict={}
+            given=req.get("given")
+            numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+            for dataset in given[0]:
+                dataset2="./uploaded/"+dataset+".csv"
                 if(path.exists(dataset2)):
                     data=pd.read_csv(dataset2)
                 numerical_vars = list(data.select_dtypes(include=numerics).columns)
